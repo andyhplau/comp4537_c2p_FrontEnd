3fc654b97d1e5d2f794ec368a16950e7138d6fd0
TinLeaves <tinleaves.0@gmail.com>
2024-11-10 23:23:37 -0800

    update home

--- index.js
+++ index.js
@@ -64,7 +64,9 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
 
 
 //   ██████╗ ███████╗ ██████╗ ██╗███████╗████████╗███████╗██████╗ 
